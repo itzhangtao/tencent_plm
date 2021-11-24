92cd308ddb7f451224ab2b8103f2518418e7128a
zhangtao <itzhangtao@163.com>
2021-11-24 18:23:48 +0800

    界面开发

--- src/todo/MyToDo.vue
+++ src/todo/MyToDo.vue
@@ -21,13 +21,16 @@
         :size="'small'"
         :bordered="false"
         :expanded-row-keys.sync="expandedRowKeys"
-      />
+      >
+        <span slot="name" slot-scope="text" style="font-weight:bold;">{{ text }}</span>
+        <a-icon type="file-search" slot="details" slot-scope="text" style="font-size:18px" v-if="text != ''" />
+      </a-table>
   </div>
 
 </template>
 <script>
 const userInfos = [{
-  userName:'张涛',
+  userName:'潜龙勿动',
   deptMessage:'基金会项目管理部｜项目经理'
 }]
 const columns = [
@@ -35,67 +38,114 @@ const columns = [
     title: '流程编号',
     dataIndex: 'name',
     key: 'name',
+    width: 150,
+    scopedSlots: { customRender: 'name' },
   },
   {
     title: '流程类型',
     dataIndex: 'age',
     key: 'age',
-    width: '12%',
+    width: 100,
   },
   {
     title: '流程状态',
+    dataIndex: 'age',
+    key: 'age',
+    width: 100,
+  },
+  {
+    title: '流程详情',
+    dataIndex: 'details',
+    key: 'details',
+    width: 100,
+    scopedSlots: { customRender: 'details' },
+  },
+  {
+    title: '申请人',
+    dataIndex: 'age',
+    key: 'age',
+    width: 100,
+  },
+  {
+    title: '立项时间',
+    dataIndex: 'age',
+    key: 'age',
+    width: 150,
+  },
+  {
+    title: '项目编号',
+    dataIndex: 'age',
+    key: 'age',
+    width: 100,
+  },
+  {
+    title: '项目名称',
     dataIndex: 'address',
-    width: '30%',
+    width: 100,
     key: 'address',
+  },
+  {
+    title: '当前流程节点',
+    dataIndex: 'age',
+    key: 'age',
+    width: 100,
   },
 ];
 
 const data = [
   {
     key: 1,
-    name: '待办',
+    name: '待处理',
+    details:'',
     children: [
       {
         key: 11,
-        name: 'John Brown',
-        age: 42,
-        address: 'New York No. 2 Lake Park',
+        name: 'PA-20210808-SGCRK',
+        age: '项目立项申请',
+        address: 5000,
+        details:'详情',
       },
       {
         key: 12,
-        name: 'John Brown jr.',
-        age: 30,
-        address: 'New York No. 3 Lake Park',
+        name: 'PA-20210808-SGCRK',
+        age: '项目立项申请',
+        address: 5000,
+        details:'详情',
       },
       {
         key: 13,
-        name: 'Jim Green sr.',
-        age: 72,
-        address: 'London No. 1 Lake Park',
+        name: 'PA-20210808-SGCRK',
+        age: '项目立项申请',
+        address: 5000,
+        details:'详情',
       },
     ],
   },
   {
     key: 2,
-    name: '已办',
+    name: '已处理',
+    details:'',
     children: [
       {
         key: 11,
-        name: 'John Brown',
-        age: 42,
-        address: 'New York No. 2 Lake Park',
+        name: 'PA-20210808-SGCRK',
+        age: '项目立项申请',
+        address: 5000,
+        details:'详情',
       },
       {
         key: 12,
-        name: 'John Brown jr.',
-        age: 30,
-        address: 'New York No. 3 Lake Park',
+        name: 'PA-20210808-SGCRK',
+        age: '项目立项申请',
+        address: 5000,
+        details:'详情',
       },
       {
         key: 13,
-        name: 'Jim Green sr.',
-        age: 72,
-        address: 'London No. 1 Lake Park',
+        name: 'PA-20210808-SGCRK',
+        age: '项目立项申请',
+        address: 5000,
+        details:'详情',
       },
     ],
   },
@@ -107,9 +157,27 @@ export default {
       data,
       columns,
       userInfos,
-      expandedRowKeys: [],
+      expandedRowKeys: [1,2],
     };
   },
+  methods:{
+    expandIcon(props){
+       if(props.record.children.length > 0){
+            if (props.expanded) {
+                return <a style="color: 'black',margin-right:10px" onClick={e => {
+                    props.onExpand(props.record, e);
+                }}><a-icon type="caret-down" /></a>
+            } else {
+                return <a style="color: 'black' ,margin-right:10px" onClick={e => {
+                    props.onExpand(props.record, e);
+                }}><a-icon type="caret-right" /></a>
+            }
+        }else{
+            return <span style="margin-right:10px"><a-icon type="smile" /></span>
+        }
+     
+    },
+  }
 };
 </script>
 
